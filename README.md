10797af20c2233ef3971f463f8cfeadedc7c8c72
smit kunpara <smitkunpara@gmail.com>
2023-07-12 21:32:16 +0530

    fix gitignore error

--- README.md
+++
@@ -1,1 +0,0 @@
-# AgriRent
--- home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import shared_equipment,taken_equipment
-
-
-admin.site.register(shared_equipment)
-admin.site.register(taken_equipment)
